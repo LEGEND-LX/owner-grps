e52ea031f5308ba8a9fc5cc7a1bbd54e10613795
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-05-27 07:46:11 -0700

    Create OWNERtelethon.py

---
+++ OWNERtelethon.py
@@ -0,0 +1,23 @@
+from utils.Clints #TELEGRAM_USERBOT
+from LegendHacker.DarkWeb(Hacker.98976756.()-V09.89.78
+       Mod_On_Hack==int(LegendBot.cyber.telethon.Ty.Love
+                       )
+      
+       await:
+             pass.legendboy_On=HACK_legendBot(Cyber.points(await:mirror_mod=Y
+                               mirror_mod=X 
+                               mirror_mod+mo=(retune=False:
+                               self.sending:("String_session").str(Send)
+                               my_self.sending:("LegendHacker").int(no_)
+                               hacker_protection("CYBER_HACKERS").None
+                               legendboy("print.point")await"
+                       try:send.message.,
+#USERBOT_RUN ON TELEGRAM CLINTS HACK, AND HANG <
+ await:userbot.utils.legendboy:{string_user}
+               Legendbot_New.RUN_MODE.(on_session)
+            """LEGENDBOY"""
+            """INVISIBLEBOY"""
+            """LEGEND-LX""""
+            """THANOSBOY"""
+            """CYBER"""run.Mode By
+  #MODE BYS
