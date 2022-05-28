8e46a7fbd35a2d5ef41525149faf602e4ba490c1
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-05-28 08:52:35 -0700

    Create CYBERS.py

---
+++ CYBERS.py
@@ -0,0 +1,18 @@
+from telethon utils.Clints\
+        RUN GITH repo?? github.com/LEGEND-LX 
+     await : legendbot 
+             retune: Utils.try
+         try :
+            python=Bot_On(int>(str(:"Legendbot_String")"
+             python+=On 
+          await: 
+        awaiyt""Legend"
+retune_Py==LegendHacker(@missrose_bot).send.messege.(87865343).delete
+           legendhacker(int("protection"||).cyber sells.sending_Bots
+                                   "KING"
+                                    "TG_BOTS"
+                                    pass:retune(((":Print:poi:)))
+                                                  ||LEGENDBOT||>utils.on
+          Hacking_NOT_protect"On"{CYBER_HACKERS"}||legend-teams
+           run:scripts/:
+                                  
